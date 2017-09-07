b341a711d34fc36d86b0cf56802eeb7bc3921350
Ortega <Ortega@DESKTOP-97QNAH9>
2017-09-07 09:50:21 +0200

    Add project files.

---
+++ HTTP_SocketServer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTP_SocketServer
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Starting server...");
+            TcpListener listener = new TcpListener(IPAddress.Any, 11000);
+            listener.Start();
+
+            while (true)
+            {
+                Socket client = listener.AcceptSocket();
+                Server s = new Server(client);
+                s.Run();
+            }
+        }
+    }
+}
+
+
+
+
+
+
+            //if (client.Connected)
+            //{
+            //    while (true)
+            //    {
+            //        string request = sr.ReadLine();
+            //        switch (request)
+            //        {
+            //            case "GET /date HTTP/1.1":
+            //                break;
+            //        }
+            //    }
+
+//}
---
+++ HTTP_SocketServer/Server.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HTTP_SocketServer
+{
+    public class Server
+    {
+        Socket client;
+        public Server(Socket client)
+        {
+            this.client = client;
+
+        }
+
+
+        public void Run()
+        {
+            NetworkStream ns = new NetworkStream(client);
+            StreamWriter sw = new StreamWriter(ns, Encoding.ASCII) { AutoFlush = true };
+            StreamReader sr = new StreamReader(ns, Encoding.ASCII);
+
+            if (client.Connected)
+            {
+                while (true)
+                {
+                    string request = sr.ReadLine();
+                    if (request != null)
+                    {
+                        switch (request)
+                        {
+                            case "GET /date HTTP/1.1":
+                                break;
+                        }
+                    }
+                   
+                }
+
+            }
+        }
+            
+
+     }
+
+    }
+
